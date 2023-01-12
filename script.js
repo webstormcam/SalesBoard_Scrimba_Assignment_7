bf5677b0c163f82d3d3d1a4f06b32e45fa25917a
Cameron Chandler <webstormcam@gmail.com>
2023-01-11 21:41:37 -0500

    JS is coming together!

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 
-let arrayOfSales=[];
-let arrayOfAchivements=[];
+let arrayOfSales=[]
+let arrayOfAchivements=[]
 let salesNum = document.getElementById('live-sales-num')
 let salesOutput = document.getElementById('sales-output')
 let achivementNum = document.getElementById('live-achievement-num')
 let revenueOutput= document.getElementById('revenue')
 let commisionOutput = document.getElementById('commision')
+let revenueTotal = 0
+let commisionTotal = 0 
 
 // Product A info
 let productA = {
@@ -14,6 +16,8 @@ let productA = {
     commision: 50,
     sellA:function(){
         arrayOfSales.push(this.emoji)
+        revenueTotal+=this.revenue
+        commisionTotal+=this.commision
         render()
     }
 }
@@ -25,7 +29,8 @@ let productB = {
     commision: 75,
     sellB:function(){
         arrayOfSales.push(this.emoji)
-      
+        revenueTotal+=this.revenue
+        commisionTotal+=this.commision
         render()
       
 
@@ -35,12 +40,14 @@ let productB = {
 
 function render(){
     let salesHolder=""
-    for(let i=0;i<arrayOfSales.length;i++){
+    for(let i=0; i<arrayOfSales.length; i++){
+
         salesHolder+=arrayOfSales[i]
     }
-    salesNum.innerHTML=arrayOfSales.length
-    salesOutput.innerHTML=salesHolder
 
-
-    
+    salesNum.innerHTML = arrayOfSales.length
+    salesOutput.innerHTML = salesHolder
+    revenueOutput.innerHTML = `$${revenueTotal}`
+    commisionOutput.innerHTML = `$${commisionTotal}`
+ 
 }
