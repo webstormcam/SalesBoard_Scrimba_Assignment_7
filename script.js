74161a43c2a51304412c7dd189a59c9f3e932f31
Cameron Chandler <webstormcam@gmail.com>
2023-01-11 21:16:50 -0500

    Got the lives sales to work

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
+
+let arrayOfSales=[];
+let arrayOfAchivements=[];
+let salesNum = document.getElementById('live-sales-num')
+let salesOutput = document.getElementById('sales-output')
+let achivementNum = document.getElementById('live-achievement-num')
+
 // Product A info
 let productA = {
     emoji: "⭐",
     revenue: 200,
-    commision: 50
+    commision: 50,
+    sellA:function(){
+        arrayOfSales.push(this.emoji)
+        render()
+    }
 }
 
 // Product B info
 let productB = {
     emoji: "🔥",
     revenue: 300,
-    commision: 75
+    commision: 75,
+    sellB:function(){
+        arrayOfSales.push(this.emoji)
+        render()
+      
+
+    }
+}
+
+
+function render(){
+    let salesHolder=""
+    for(let i=0;i<arrayOfSales.length;i++){
+        salesHolder+=arrayOfSales[i]
+    }
+    salesNum.innerHTML=arrayOfSales.length
+    salesOutput.innerHTML=salesHolder
+
+
+    
 }
